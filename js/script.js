17889d2a93761471aefc3c7f324bb52f9e73ba96
Thanh Nguyen <ntthanhgl@gmail.com>
2020-09-22 11:41:10 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+
 $( document ).ready(function() {
-    
     /* progress bar */
     if($('#loadingBar').length > 0){
         var i = 0;
@@ -21,7 +21,52 @@ $( document ).ready(function() {
             
         }
     }
+    /* script run select option */
+    if($('select').length > 0){
+        $('select').niceSelect();
+    }
 
-    $('select').niceSelect();
+    /* Begin script run side bar */
+    $('.panel-menu .accordion').on('show.bs.collapse', function (e) {
+        $(e.target).prev('.card-header').addClass('active');
+    })
+   
+    $('.panel-menu .accordion').on('hide.bs.collapse', function (e) {
+       $(this).find('.card-header').not($(e.target)).removeClass('active');
+    });
+
+    $('.navbar-item-toggler').on('click', function(){
+        $('.sidebar').addClass("open");
+    })
+    $('.close-sidebar').on('click',function(){
+        $('.sidebar').removeClass("open");
+    })
+    /* End script run side bar */
+
+    /* smartwizard */
+    if($('#smartwizard').length > 0){
+        $('#smartwizard').smartWizard({
+            enableURLhash: false,
+            anchorSettings: {
+                anchorClickable: true,
+                enableAllAnchors: true, 
+                markDoneStep: true,
+                markAllPreviousStepsAsDone: true,
+                removeDoneStepOnNavigateBack: false,
+                enableAnchorOnDoneStep: true
+            },
+            lang: { 
+                next: 'Tương lai',
+                previous: 'Quá khứ'
+            },
+        });
+    }
+
+    if($('#imagesSlider').length > 0){
+        $('#imagesSlider').refineSlide({
+            maxWidth: 500
+        });
+    }
+    
 
 });
